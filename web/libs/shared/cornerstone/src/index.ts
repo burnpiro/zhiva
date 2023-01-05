8aec4f6738a5c69709a766f9abb697ab30756e84
burnpiro <kemalpiro@gmail.com>
2023-01-05 23:03:06 +0200

    Export new metadata generators

--- web/libs/shared/cornerstone/src/index.ts
+++ web/libs/shared/cornerstone/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/initCornerstone';
 export * from './lib/validNumber';
 export * from './lib/urlParser';
 export * from './lib/generateUID';
+export * from './lib/MetadataProvider';
+export * from './lib/extractInstanceData';
