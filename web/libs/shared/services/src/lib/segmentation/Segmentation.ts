2c1d0265aeb9ae23f5ac59dc7e42f1f4d0f4144d
Kemal Erdem <kemalpiro@gmail.com>
2023-06-01 20:01:26 +0200

    Segmentation Class

--- web/libs/shared/services/src/lib/segmentation/Segmentation.ts
+++ web/libs/shared/services/src/lib/segmentation/Segmentation.ts
@@ -1,6 +1,7 @@
 import * as cornerstoneTools from '@cornerstonejs/tools';
-import {volumeLoader, cache, ImageVolume} from '@cornerstonejs/core';
-import {SegmentationRepresentations} from "@cornerstonejs/tools/dist/esm/enums";
+import { volumeLoader, cache, ImageVolume } from '@cornerstonejs/core';
+import { Enums as CSToolsEnums } from '@cornerstonejs/tools';
+import { BrushColorClass, DEFAULT_SEG_LABEL } from '@zhiva/utils';
 
 const {
   ToolGroupManager,
@@ -10,28 +11,53 @@ const {
 
 interface SegmentationProps {
   id: string;
-  type?: SegmentationRepresentations;
+  type?: CSToolsEnums.SegmentationRepresentations;
+  label?: string;
 }
 
 class Segmentation {
   readonly id: string;
-  public type: SegmentationRepresentations;
+  public type: CSToolsEnums.SegmentationRepresentations;
+  public assignedVolume?: string;
+  public activeSegmentIndex: number;
+  public segmentationRepresentationUID?: string;
+  public label: string;
+  public colorLUTIndex: number;
 
-  constructor({ id, type }: SegmentationProps) {
+  constructor({ id, type, label }: SegmentationProps) {
     this.id = id;
     this.type = type || csToolsEnums.SegmentationRepresentations.Labelmap;
+    this.activeSegmentIndex = 0;
+    this.colorLUTIndex = 0;
+    this.label = label || DEFAULT_SEG_LABEL;
   }
 
-  public async initializeSegmentationFromVolume(volumeId: string): Promise<ImageVolume> {
-    const existingVolume = cache.getVolume(this.id);
+  public async initializeSegmentationFromVolume(
+    volumeId: string,
+    replaceIfExists: boolean = false
+  ): Promise<ImageVolume> {
+    this.assignVolumeIdToSegmentation(volumeId);
+    const existingVolume = cache.getVolume(this.id) as ImageVolume;
 
-    if (existingVolume) {
+    if (existingVolume && !replaceIfExists) {
       return existingVolume;
+    }
+
+    if (existingVolume && replaceIfExists) {
+      cache.removeVolumeLoadObject(this.id);
     }
 
     return volumeLoader.createAndCacheDerivedVolume(volumeId, {
       volumeId: this.id,
     });
+  }
+
+  public assignVolumeIdToSegmentation(volumeId: string): void {
+    this.assignedVolume = volumeId;
+  }
+
+  public setBrushClass(brushClass: BrushColorClass) {
+    this.colorLUTIndex = brushClass.activeLabelmapIndex;
   }
 }
 
