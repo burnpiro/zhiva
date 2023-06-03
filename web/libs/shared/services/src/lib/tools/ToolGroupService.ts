2199a1cb4308a359b43d34da7d60becfb3e4fc77
Kemal Erdem <kemalpiro@gmail.com>
2023-06-03 22:34:05 +0200

    Tool Grouping

--- web/libs/shared/services/src/lib/tools/ToolGroupService.ts
+++ web/libs/shared/services/src/lib/tools/ToolGroupService.ts
@@ -1,39 +1,142 @@
 import * as cornerstoneTools from '@cornerstonejs/tools';
-const {
-  SegmentationDisplayTool,
-  ToolGroupManager,
-  Enums: csToolsEnums,
-  segmentation,
-} = cornerstoneTools;
+const { segmentation } = cornerstoneTools;
 
 import { Segmentation } from '../segmentation';
+import { BrushColorClass } from '@zhiva/utils';
 
 class ToolGroupService {
   public static async addSegmentationsToToolGroup(
     segList: Segmentation[],
     toolGroupId: string
   ): Promise<string[]> {
-    const toolGroupRepresentation =
-      segmentation.state.getSegmentationRepresentations(toolGroupId);
-    const nonExistingSegRepresentations = segList.filter(
-      (seg) =>
-        !toolGroupRepresentation ||
-        !segmentation.state.getSegmentationRepresentationByUID(
-          toolGroupId,
-          seg.id
-        )
-    );
-    if (nonExistingSegRepresentations.length > 0) {
-      return segmentation.addSegmentationRepresentations(
+    ToolGroupService.removeSegmentationsFromToolGroup(toolGroupId, segList);
+    if (segList.length > 0) {
+      console.log(segList);
+      const representations = await segmentation.addSegmentationRepresentations(
         toolGroupId,
-        nonExistingSegRepresentations.map((seg) => ({
+        segList.map((seg) => ({
           segmentationId: seg.id,
-          type: seg.type,
+          type: seg.type
         }))
       );
+      for (const seg of segList) {
+        console.log('set segcolor ', seg.id, seg.colorLUTIndex)
+        ToolGroupService.setSegmentationColor(
+          toolGroupId,
+          seg,
+          seg.colorLUTIndex
+        );
+      }
+      return representations;
     }
-
     return [];
+  }
+
+  public static removeSegmentationsFromToolGroup(
+    toolGroupId: string,
+    segList: Segmentation[]
+  ) {
+    const toolGroupRepresentations =
+      segmentation.state.getSegmentationRepresentations(toolGroupId) || [];
+
+    const existingSegRepresentations = segList.filter(
+      (seg) =>
+        toolGroupRepresentations.length !== 0 ||
+        toolGroupRepresentations
+          .map((rep) => rep.segmentationId)
+          .includes(seg.id)
+    );
+
+    if (existingSegRepresentations.length > 0) {
+      segmentation.removeSegmentationsFromToolGroup(
+        toolGroupId,
+        existingSegRepresentations
+          .filter((seg) =>
+            toolGroupRepresentations.find(
+              (rep) => rep.segmentationId === seg.id
+            )
+          )
+          .map(
+            (seg) =>
+              toolGroupRepresentations.find(
+                (rep) => rep.segmentationId === seg.id
+              )?.segmentationRepresentationUID || ''
+          )
+      );
+    }
+  }
+
+  public static setSegmentationVisibility(
+    toolGroupId: string,
+    segObj: Segmentation,
+    visibility: boolean
+  ): void {
+    const toolGroupRepresentations =
+      segmentation.state.getSegmentationRepresentations(toolGroupId) || [];
+
+    const segRepresentation = toolGroupRepresentations.find(
+      (rep) => rep.segmentationId === segObj.id
+    );
+
+    if (segRepresentation) {
+      segmentation.config.visibility.setSegmentationVisibility(
+        toolGroupId,
+        segRepresentation.segmentationRepresentationUID,
+        visibility
+      );
+    } else {
+      console.warn(
+        `Trying to set visibility to non-existing segmentation: ${segObj.id}`
+      );
+    }
+  }
+
+  public static setActiveSegmentation(
+    toolGroupId: string,
+    segObj: Segmentation
+  ): void {
+    const toolGroupRepresentations =
+      segmentation.state.getSegmentationRepresentations(toolGroupId) || [];
+
+    const segRepresentation = toolGroupRepresentations.find(
+      (rep) => rep.segmentationId === segObj.id
+    );
+
+    if (segRepresentation) {
+      segmentation.activeSegmentation.setActiveSegmentationRepresentation(
+        toolGroupId,
+        segRepresentation.segmentationRepresentationUID
+      );
+    } else {
+      console.warn(
+        `Trying to set visibility to non-existing segmentation: ${segObj.id}`
+      );
+    }
+  }
+
+  public static setSegmentationColor(
+    toolGroupId: string,
+    segObj: Segmentation,
+    activeLabelmapIndex: BrushColorClass['activeLabelmapIndex']
+  ): void {
+    const toolGroupRepresentations =
+      segmentation.state.getSegmentationRepresentations(toolGroupId) || [];
+
+    const segRepresentation = toolGroupRepresentations.find(
+      (rep) => rep.segmentationId === segObj.id
+    );
+
+    if (segRepresentation) {
+      segmentation.config.color.setColorLUT(
+        toolGroupId,
+        segRepresentation.segmentationRepresentationUID,
+        activeLabelmapIndex
+      );
+    } else {
+      console.warn(
+        `Trying to set color to non-existing segmentation: ${segObj.id}`
+      );
+    }
   }
 }
 
