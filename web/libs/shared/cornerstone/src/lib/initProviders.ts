bd6cc762c8152e7aa4da089460ea247664e5241a
Kemal Erdem <kemalpiro@gmail.com>
2023-07-28 15:42:40 +0200

    initial data providers

--- web/libs/shared/cornerstone/src/lib/initProviders.ts
+++ web/libs/shared/cornerstone/src/lib/initProviders.ts
@@ -1,26 +1,31 @@
 import * as cornerstone from '@cornerstonejs/core';
-import WADORSHeaderProvider from './WADORSHeaderProvider';
-import ptScalingMetaDataProvider from './ptScalingMetaDataProvider';
-import metadataProvider from "./MetadataProvider";
+import {WADORSHeaderProvider} from './WADORSHeaderProvider';
+import {ptScalingMetaDataProvider} from './ptScalingMetaDataProvider';
+import metadataProvider from './MetadataProvider';
+import { wadoRsMetaDataProvider } from './wadoRsMetaDataProvider';
+import { wadoUriMetaDataProvider } from './wadoUriMetaDataProvider';
 
 const { calibratedPixelSpacingMetadataProvider } = cornerstone.utilities;
 
 export default function initProviders() {
-  // cornerstone.metaData.addProvider(
-  //   WADORSHeaderProvider.get.bind(WADORSHeaderProvider),
-  //   9999
-  // );
-  // cornerstone.metaData.addProvider(
-  //   ptScalingMetaDataProvider.get.bind(ptScalingMetaDataProvider),
-  //   10000
-  // );
-  // cornerstone.metaData.addProvider(
-  //   // @ts-ignore
-  //   calibratedPixelSpacingMetadataProvider.get.bind(
-  //     calibratedPixelSpacingMetadataProvider
-  //   ),
-  //   11000
-  // );
+  // cornerstone.metaData.addProvider(wadoRsMetaDataProvider, 1);
+  // @ts-ignore
+  cornerstone.metaData.addProvider(wadoUriMetaDataProvider, 10000);
+  cornerstone.metaData.addProvider(
+    WADORSHeaderProvider.get.bind(WADORSHeaderProvider),
+    9999
+  );
+  cornerstone.metaData.addProvider(
+    ptScalingMetaDataProvider.get.bind(ptScalingMetaDataProvider),
+    10000
+  );
+  cornerstone.metaData.addProvider(
+    // @ts-ignore
+    calibratedPixelSpacingMetadataProvider.get.bind(
+      calibratedPixelSpacingMetadataProvider
+    ),
+    11000
+  );
   // cornerstone.metaData.addProvider(
   //   metadataProvider.get.bind(
   //     metadataProvider
