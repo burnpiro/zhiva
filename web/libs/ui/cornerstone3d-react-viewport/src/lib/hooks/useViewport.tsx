4156f83496ba1ad6af8fae4c094f4c5cbe28e947
Kemal Erdem <kemalpiro@gmail.com>
2023-08-26 21:46:40 +0200

    Destroy viewport after leaving the view

--- web/libs/ui/cornerstone3d-react-viewport/src/lib/hooks/useViewport.tsx
+++ web/libs/ui/cornerstone3d-react-viewport/src/lib/hooks/useViewport.tsx
@@ -1,18 +1,15 @@
 import { RefObject, useEffect, useState } from 'react';
-import { RenderingEngine } from '@cornerstonejs/core';
+import { RenderingEngine, Types as CSTypes } from '@cornerstonejs/core';
 import { Cornerstone3dReactViewportProps } from '../Cornerstone3dReactViewportProps';
-import {
-  IStackViewport,
-  IVolumeViewport,
-} from '@cornerstonejs/core/dist/esm/types';
+import {ToolGroupManager} from "@cornerstonejs/tools";
 
 export function useViewport(
   renderingEngine?: RenderingEngine,
   element?: RefObject<HTMLDivElement>,
   viewport?: Cornerstone3dReactViewportProps['viewport']
-): IStackViewport | IVolumeViewport | undefined {
+): CSTypes.IStackViewport | CSTypes.IVolumeViewport | undefined {
   const [viewportInstance, setViewportInstance] = useState<
-    IStackViewport | IVolumeViewport
+    CSTypes.IStackViewport | CSTypes.IVolumeViewport
   >();
 
   useEffect(() => {
@@ -25,14 +22,45 @@ export function useViewport(
       };
       // console.log(viewportProps);
       const currViewport = renderingEngine.getViewport(viewport.id);
-      if(currViewport && currViewport.element === element.current && currViewport.type === viewport.type && JSON.stringify(viewportProps.defaultOptions) === JSON.stringify(currViewport.options)) {
+      if (
+        currViewport &&
+        currViewport.element === element.current &&
+        currViewport.type === viewport.type &&
+        JSON.stringify(viewportProps.defaultOptions) ===
+          JSON.stringify(currViewport.options)
+      ) {
         // If viewport with the same props and assigned to the same element exists
         // Do not re-enable the same viewport, cornerstone recreated instance from scratch, and we don't want to update object if it's the same object
         return;
       }
+      console.log('enable element')
       renderingEngine.enableElement(viewportProps);
       setViewportInstance(renderingEngine.getViewport(viewport.id));
     }
+
+    return () => {
+      if (viewport?.id && renderingEngine && !element?.current) {
+        const toolGroupInstance = ToolGroupManager.getToolGroupForViewport(viewport.id, renderingEngine.id);
+
+
+        if(toolGroupInstance) {
+          toolGroupInstance.removeViewports(renderingEngine.id, viewport.id);
+        }
+
+        const currViewport = renderingEngine.getViewport(viewport.id);
+        if (
+          currViewport &&
+          currViewport.type === viewport.type &&
+          JSON.stringify(viewport.options) ===
+            JSON.stringify(currViewport.options)
+        ) {
+          console.log('disable element')
+          currViewport.removeAllActors();
+          renderingEngine.disableElement(viewport.id);
+          setViewportInstance(undefined);
+        }
+      }
+    };
   }, [viewport, element?.current, renderingEngine]);
 
   return viewportInstance;
